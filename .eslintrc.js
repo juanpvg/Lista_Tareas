a309f4eaeedc46264436e1856c2e529eb530e315
juanpvg <juanpablovasgon@gmail.com>
2023-07-24 10:55:11 -0500

    feature/issue3/FilesConfiguration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                project: 'tsconfig.json',
-                "sourceType": "tsconfig.json"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
+export default {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
         project: 'tsconfig.json',
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+        sourceType: 'tsconfig.json'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
 }
